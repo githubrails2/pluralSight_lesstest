7af0052760325881475802f427000c994a3172b1
Bill White <55554500+githubrails2@users.noreply.github.com>
2021-03-04 22:57:44 -0600

    last few files
    Last few files restricted from using git so will pull and restructure

---
+++ authorsReducer.js
@@ -0,0 +1,11 @@
+import initialState from "../initialState";
+import * as types from "../actions/actionTypes";
+const authorsReducer = (state = initialState.authors, action) => {
+	switch (action.type) {
+		case types.LOAD_AUTHORS_SUCCESS:
+			return action.authors;
+		default:
+			return state;
+	}
+};
+export default authorsReducer;
--- index.js
+++ index.js
@@ -1,19 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
-import { Provider as ReduxProvider } from "react-redux";
-import configureStore from "./redux/configureStore";
-
-const store = configureStore();
-
-ReactDOM.render(
-	<ReduxProvider store={store}>
-		<Router>
-			<App />
-		</Router>
-	</ReduxProvider>,
-	document.getElementById("root")
-);
+import { combineReducers } from "redux";
+import authors from "./authorsReducer";
+import courses from "./coursesReducer";
+import apiCallsInProgress from "./apiStatusReducer";
+const rootReducer = combineReducers({
+	courses,
+	authors,
+	apiCallsInProgress,
+});
+export default rootReducer;
